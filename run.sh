c5768721b3b3c5bbdd0a8c4a29d596ca0e8916f4
ogabrielinacio <gabrieldev@disroot.org>
2023-12-03 13:30:47 -0300

    feat: add python fibonacci server

---
+++ run.sh
@@ -0,0 +1,1 @@
+yarn dev & python3 -m flask --app venv/src/index.py run
---
+++ venv/src/index.py
@@ -0,0 +1,39 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+def recursive_fibonacci(n):
+    # python sucks in recursive programming
+    if n == 0 or n == 1:
+        return n
+    return recursive_fibonacci(n - 1) + recursive_fibonacci(n - 2)
+
+def non_recursive_fibonacci(n):
+    if n < 0:
+            raise ValueError("n must be non-negative")
+    elif n == 0:
+        return 0
+    elif n == 1:
+        return 1
+
+    previous, current = 0, 1
+    for i in range(2, n + 1):
+        next = previous + current
+        previous = current
+        current = next
+
+    return current
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/nonrecursive", methods=["POST"])
+def non_rec_fibonacci():
+    n = request.args.get('n', default=0, type=int)
+    return f'\n\n Fibonacci de {n} = {non_recursive_fibonacci(n)} \n\n'
+
+@app.route("/recursive", methods=["POST"])
+def rec_fibonacci():
+    n = request.args.get('n', default=0, type=int)
+    return f'\n\n Fibonacci de {n} = {recursive_fibonacci(n)} \n\n'
+
